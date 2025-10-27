a26c35e89bb1971f4c6de90fbf4daa01434a54e2
Chayil Mauristhene <chayilmauristhene@Chayils-MacBook-Pro.local>
2025-10-27 16:44:06 -0400

    Added comments to make code readable

--- Initial_Game_Baseline/indexfinal.js
+++ Initial_Game_Baseline/indexfinal.js
@@ -1,37 +1,56 @@
-const canvas = document.querySelector ('canvas')
+// =============
+// CANVAS SETUP
+// =============
+const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
-
-const countdownEl = document.getElementById('countdown')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+// ======================
+// HTML ELEMENTS ELEMENTS
+// ======================
 const scoreEl = document.querySelector('#scoreEl')
-const startGameBtn = document.querySelector('#startGameBtn')
 const modalEl = document.querySelector('#modalEl')
 const bigScoreEl = document.querySelector('#bigScoreEl')
 
+// ===============
+// GAME VARIABLES
+// ===============
+const friction = 0.99
+const gravity = -0.99
+let particles = []
+let projectiles = []
 
+let score = 0
+let countdown = 30
+let made_basket_dist = 6
+let animationID
+
+// Represents the player class
 class Player {
-    constructor (x, y, radius, color) {
+    constructor(x, y, radius, color) {
         this.x = x
         this.y = y
         this.radius = radius
         this.color = color
     }
 
-    draw(){
+    draw() {
         c.beginPath()
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         c.fillStyle = this.color
         c.fill()
     }
+
     update() {
         this.draw()
     }
-
 }
 
+// ===========================
+// PROJECTILE CLASS
+// ===========================
 class Projectile {
     constructor(x, y, radius, color, velocity) {
         this.x = x
@@ -47,16 +66,17 @@ class Projectile {
         c.fillStyle = this.color
         c.fill()
     }
+
     update() {
         this.draw()
-        this.x = this.x + this.velocity.x
-        this.y = this.y + this.velocity.y
+        this.x += this.velocity.x
+        this.y += this.velocity.y
     }
 }
 
+// Represents the particle class for the splash effect
 class Particle {
     constructor(x, y, radius, color, velocity) {
-
         this.x = x
         this.y = y
         this.radius = radius
@@ -74,20 +94,20 @@ class Particle {
         c.fill()
         c.restore()
     }
+
     update() {
         this.draw()
         this.velocity.x *= friction
         this.velocity.y *= friction
-        this.x = this.x + this.velocity.x
-        this.y = this.y + this.velocity.y
+        this.x += this.velocity.x
+        this.y += this.velocity.y
         this.alpha -= 0.01
     }
 }
 
-let particles = []
-
+//Represents the basket class the player/ basketball will be aiming for
 class Basket {
-    constructor (w, h, color) {
+    constructor(w, h, color) {
         this.w = w
         this.h = h
         this.color = color
@@ -95,7 +115,12 @@ class Basket {
 
     draw() {
         c.beginPath()
-        c.rect( (canvas.width / 2) - (this.w / 2), (canvas.height / 2) - (this.h / 2), this.w, this.h);
+        c.rect(
+            (canvas.width / 2) - (this.w / 2),
+            (canvas.height / 2) - (this.h / 2),
+            this.w,
+            this.h
+        )
         c.fillStyle = this.color
         c.fill()
     }
@@ -105,115 +130,94 @@ class Basket {
     }
 }
 
-
-
+// ===========================
+// INITIAL OBJECTS
+// ===========================
 let basket = new Basket(125, 25, "blue")
 
+// random starting x for player, but set y
 let x = 10 + (Math.random() * canvas.width - 10)
 let y = canvas.height / 1.15
+const player = new Player(x, y, 40, 'rgb(200,90,0)')
 
-const player = new Player (x, y, 40, 'rgb(200,90,0)')
-
-const friction = .99
-
-
+// template projectile (radius will decrease on click)
 const projectile = new Projectile(
-    canvas.width / 2, 
-    canvas.height / 1.15, 
-    5, 
-    'rgb(200,90,0)', 
-    {
-        x: 1, 
-        y: 1
-    }
+    canvas.width / 2,
+    canvas.height / 1.15,
+    5,
+    'rgb(200,90,0)',
+    { x: 1, y: 1 }
 )
 
-const projectiles = []
-
-const gravity = - 0.99
-
-let animationID
-let score = 0
-let countdown = 30
-let made_basket_dist = 6
-
+//Intended to loop through the animation for as long as the time is not 0
 function animate() {
-    requestAnimationFrame(animate)
+    animationID = requestAnimationFrame(animate)
+
+    // Clear screen with slight fade for trail effect
     c.fillStyle = 'rgba(0, 0, 0, 0.1)'
     c.fillRect(0, 0, canvas.width, canvas.height)
+
     basket.draw()
-    projectiles.forEach((projectile, index) => {
-        projectile.velocity.y -= gravity
-        projectile.update()
+
+    // Update all projectiles
+    projectiles.forEach((proj) => {
+        proj.velocity.y -= gravity
+        proj.update()
     })
+
     player.draw()
 
-    projectiles.forEach((projectile, projectileIndex) =>  {
-            const dist = Math.hypot(projectile.x - canvas.width / 2, 
-            projectile.y - canvas.height / 2 
-            )
-
-        //when projectiles hits top of basket
-        if (dist - projectile.radius < made_basket_dist && projectile.velocity.y > 0 ) {
-            console.log("basket made!")
-            console.log(dist)
-            console.log(projectile.velocity.y)
-
-            //increase the score
+    // Collision detection for projectiles and basket
+    projectiles.forEach((proj, index) => {
+        const dist = Math.hypot(proj.x - canvas.width / 2, proj.y - canvas.height / 2)
+        if (dist - proj.radius < made_basket_dist && proj.velocity.y > 0) {
+            // Basket made
             score += 2
             scoreEl.innerHTML = score
 
-
-            //splash effect on made basket
+            // Splash particles
             for (let i = 0; i < 20; i++) {
                 particles.push(
-                    new Particle(projectile.x, 
-                        projectile.y, 
-                        Math.random() * 5 , 
-                        "purple", 
-                        {
-                        x: (Math.random() - .5) * (Math.random() * 15), 
+                    new Particle(proj.x, proj.y, Math.random() * 5, "purple", {
+                        x: (Math.random() - 0.5) * (Math.random() * 15),
                         y: (Math.random() - 1) * (Math.random() * 15)
                     })
-                    )
+                )
             }
 
-            projectiles.splice(projectileIndex, 1)
+            // Remove projectile
+            projectiles.splice(index, 1)
         }
     })
-    particles.forEach ((particle, index) => {
+
+    // Update particles
+    particles.forEach((particle, index) => {
         if (particle.alpha <= 0) {
             particles.splice(index, 1)
         } else {
             particle.update()
         }
-    });
+    })
 }
 
-//start of timer code
+// Code related to the timer counting down
 const timeH = document.querySelector('h1')
 let timeSecond = 10
-
 timeH.innerHTML = `00:${timeSecond}`
 
-const countDown = setInterval (() => {
+const countDown = setInterval(() => {
     timeSecond--
     displayTime(timeSecond)
-    timeH.innerHTML = `00:${timeSecond}`
-    if (timeSecond <= 9 || timeSecond < 1) {
-        timeH.innerHTML = `00:0${timeSecond}`
-    }
-    if (timeSecond <= 0 || timeSecond < 1) {
+    if (timeSecond <= 0) {
         endTime()
         clearInterval(countDown)
     }
-},1000)
+}, 1000)
 
 function displayTime(second) {
     const min = Math.floor(second / 60)
     const sec = Math.floor(second % 60)
-    timeH.innerHTML = `${min < 10 ? '0' : ''} ${min} : ${sec < 10 ? '0' : ''}${sec}`
-
+    timeH.innerHTML = `${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec}`
 }
 
 function endTime() {
@@ -221,10 +225,9 @@ function endTime() {
     cancelAnimationFrame(animationID)
     modalEl.style.display = 'flex'
 }
-//end of timer code
 
+// Click event listener for the player to shoot when they click
 addEventListener('click', (event) => {
-
     const angle = Math.atan2(
         event.clientY - canvas.height / 1.15,
         event.clientX - player.x
@@ -235,29 +238,18 @@ addEventListener('click', (event) => {
         y: Math.sin(angle) * 35
     }
 
-    if (timeSecond <= 0 || timeSecond < 1) {
-        endTime()
-        clearInterval(countDown)
-    }
-
+    // Add projectile
     projectile.radius = 40
-
     projectiles.push(
-        new Projectile(
-            player.x, 
-            canvas.height / 1.15, 
-            projectile.radius -= 5, 
-            'rgb(200,90,0)',
-            velocity 
-        )
+        new Projectile(player.x, canvas.height / 1.15, projectile.radius - 5, 'rgb(200,90,0)', velocity)
     )
 
-
-    let limit = 700
-    let rando_position = (limit/2) + (Math.random() * (canvas.width - limit))
-
+    // Move player randomly after shot
+    const limit = 700
+    const rando_position = (limit / 2) + (Math.random() * (canvas.width - limit))
     player.x = rando_position
     player.update()
 })
 
+//Animates the project
 animate()
